52338d07c3ffa18cadc55248c565f7bba8c2ec26
KrisTheCanadian <31254679+KrisTheCanadian@users.noreply.github.com>
2019-10-15 17:17:40 -0400

    ApartmentSale, LandSale Constructor update
    Partial Update for Apartment Sale and LandSale

--- LandSale.cpp
+++ LandSale.cpp
@@ -1,1 +1,9 @@
 #include "LandSale.h"
+
+LandSale::LandSale() 
+	:area_(-1), price_(-1)
+{}
+
+LandSale::LandSale(string, string, Client, RealEstateAgent, Date, double area, int price)
+	:area_(area), price_(price)
+{}
